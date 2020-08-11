452354d5d9ecac0412ed50259ae08cb3fd849c4f
Kenny Passenier <kennypassenier@gmail.com>
2020-08-11 19:09:56 +0200

    Unittest voor Movie klasse toegevoegd alsook de referenties naar JUnit5

---
+++ app/src/test/java/com/example/androiddevelopment_kennypassenier/models/MovieTest.java
@@ -0,0 +1,20 @@
+package com.example.androiddevelopment_kennypassenier.models;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieTest {
+    @org.junit.jupiter.api.Test
+    void testToString() {
+        Movie movie = new Movie();
+        movie.setTitle("movieTitle");
+        movie.setReleaseDate(2020);
+        assertEquals("movieTitle - 2020", movie.toString());
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTitle() {
+        Movie movie = new Movie();
+        movie.setTitle("TestMovie");
+        assertEquals("TestMovie", movie.getTitle());
+    }
+}
